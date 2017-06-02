a9c5b6694d5e3b64763b98666b3f468f880de94d
Nasheb Ismaily <nasheb@gmail.com>
2017-06-01 23:39:35 -0400

    Updated metadata

--- metadata.rb
+++ metadata.rb
@@ -3,5 +3,4 @@ maintainer       'Nasheb Ismaily'
 maintainer_email 'nasheb@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures cloudbreak-automation'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
